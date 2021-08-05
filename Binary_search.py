8416c70ac9d42c2b6dd9d9f5460615d0a39ce90d
Cadmeos <80473426+Cadmeos@users.noreply.github.com>
2021-08-05 16:48:59 -0300

    Update Binary_search.py

--- Binary_search.py
+++ Binary_search.py
@@ -11,7 +11,7 @@ def binary_search(list, item):
       high = mid - 1
     else: 
       low = mid + 1
-      return None
+    return None
       
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15,]
